3ff2dc712b0012e5b858186fd69736bbbebb728b
63csmith <63csmith@gmail.com>
2022-05-21 20:02:50 -0600

    update 1.2

--- script.js
+++ script.js
@@ -1,65 +1,74 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 //Possible Characters 
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
 ];
-let allCharacters = [upperCase, lowerCase, numbers, special];
-const confirmNumbers = document.querySelector("#number");
-const confirmUpper = document.querySelector("#uppercase");
-const confirmLower = document.querySelector("#lowercase");
-const confirmSpecial = document.querySelector("#special");
-const passwordLength = document.querySelector("#length").value;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generateRandomArray();
-  var passwordText = document.querySelector("#password");
+const confirmNumbers = document.getElementById("numbers");
+const confirmUpper = document.getElementById("uppercase");
+const confirmSpecial = document.getElementById("special");
+const confirmLength = document.getElementById("length");
+const passwordText = document.getElementById("password");
+const generateBtn = document.getElementById('generate');
+
+
+function writePassword(){
+  const totalLength = confirmLength.value;
+  const includeUppercase = confirmUpper.checked;
+  const includeNumbers = confirmNumbers.checked;
+  const includeSymbols = confirmSpecial.checked;
+  const password = generatePassword(
+    totalLength,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  );
   
-
-  passwordText.value = password;
-
 }
 
-function generatePassword() {
-  let selections = [] 
-  if(confirmUpper.checked){
-    selections.push(0)
+let generatePassword = (
+  totalLength,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+) => {
+  var selectedOptionArray = [];
+  let lcd = lowerCase;
+  selectedOptionArray.push(lowerCase);
+  console.log(lcd);
+  if (includeUppercase){
+    lcd = lcd.concat(upperCase);
+    selectedOptionArray.push(upperCase);
+  }  
+  if (includeNumbers){
+    lcd = lcd.concat(numbers);
+    selectedOptionArray.push(numbers);
+  } 
+  if (includeSymbols){
+    lcd = lcd.concat(special);
+    selectedOptionArray.push(special);
+  } 
+  console.log(selectedOptionArray)
+  const passwordCharacters = [];
+  generateRandom(selectedOptionArray,lcd,totalLength);
+  console.log(lcd)
+
+};
+function generateRandom(arr,lcd,totalLength){
+  let genArr = [];
+  for(let i =0; i < arr.length; i++){
+    genArr.push(arr[i][Math.floor(Math.random() * arr[i].length)])
     
   }
-  if(confirmLower.checked){
-    selections.push(1)
-  }  if(confirmNumbers.checked){
-    selections.push(2)
-  }  if(confirmSpecial.checked){
-    selections.push(3)
+    for (let i =0; i < totalLength - arr.length; i++){
+    genArr.push(lcd[Math.floor(Math.random() * lcd.length)])
   }
-  return selections
+
+ 
+  passwordText.innerText = genArr.join('');
 }
-  function generateRandomArray(){
-  let choiceArray = generatePassword()
-  let randomArray = []
-  for(let i=0;i<choiceArray.length; i++) {
-    randomArray.push(generateRandom(allCharacters[choiceArray[i]]))
-  } console.log (randomArray)
-}
-  
-
-function generateRandom(arr) {
-  const random = Math.floor(Math.random() * arr.length);
-console.log(random, arr[random]);
-return arr[random]
-}
-
-
-
 
 
 
 generateBtn.addEventListener("click", writePassword);
-
-
-
